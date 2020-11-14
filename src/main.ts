e686fcb1b0a81e5d60bbe224fc9619929be331bd
Wilfred <wilfred@vanderdeijl.com>
2020-11-14 22:51:54 +0100

    core only

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
+import core from '@actions/core'
+// import github from '@actions/github'
 import { wait } from './wait'
 
 async function run(): Promise<void> {
@@ -14,9 +14,10 @@ async function run(): Promise<void> {
         core.setOutput('time', new Date().toTimeString())
 
         const token = core.getInput('GITHUB_TOKEN');
-        const octokit = github.getOctokit(token);
-        const pulls = await octokit.pulls.list();
-        core.info(JSON.stringify(pulls.data, undefined, 4));
+        core.info(token);
+        // const octokit = github.getOctokit(token);
+        // const pulls = await octokit.pulls.list();
+        // core.info(JSON.stringify(pulls.data, undefined, 4));
 
     } catch (error) {
         core.setFailed(error.message)
