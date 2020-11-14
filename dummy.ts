ea7a562184410b0d5afc0ea679a73876d94dce7c
Wilfred <wilfred@vanderdeijl.com>
2020-11-14 19:20:13 +0100

    argument typings

--- dummy.ts
+++ dummy.ts
@@ -5,6 +5,28 @@
 //     body: '👋 Thanks for reporting!'
 // })
 
-module.exports = ({ _github, context }: any) => {
-    return context.payload.client_payload.value
+// github A pre-authenticated octokit/core.js client with REST endpoints and pagination plugins
+// context An object containing the context of the workflow run
+// core A reference to the @actions/core package
+// io A reference to the @actions/io package
+
+import type { GitHub } from '@actions/github/lib/utils';
+import type { context } from '@actions/github'
+import type Core from '@actions/core';
+import type Io from '@actions/io';
+
+type Octokit = InstanceType<typeof GitHub>
+type Context = typeof context;
+
+interface Arguments {
+    github: Octokit;
+    context: Context;
+    core: typeof Core;
+    io: typeof Io;
+}
+
+module.exports = (args: Arguments) => {
+    args.core.warning(JSON.stringify(args.context.payload, undefined, 4));
+    return 'foo';
+    // return args.context.payload.client_payload.value
 }
